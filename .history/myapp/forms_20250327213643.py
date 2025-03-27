a964adf40ff88ab9a951b69054e696fb56893917
Melkizedeki <mmagota2@gmail.com>
2025-03-27 22:04:35 +0300

    After styling the product form

---
+++ .history/myapp/forms_20250327213643.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Product
+
+# - Creating the forms here 
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "description", "price", "category", "photo"]
