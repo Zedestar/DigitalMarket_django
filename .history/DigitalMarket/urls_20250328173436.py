87d8b0e707b6eaadc8aa52bc327409f7e1b797bc
Melkizedeki <mmagota2@gmail.com>
2025-03-28 17:47:47 +0300

    Continuing with the authentication

--- .history/DigitalMarket/urls_20250328173436.py
+++ .history/DigitalMarket/urls_20250328173436.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    path('authenticate/', include('authentication.urls')),
+    path('authenticat/', include('authentication.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ .history/authentication/urls_20250328173217.py
@@ -0,0 +1,11 @@
+from .  import views
+from django.urls import path
+
+# - Creating the urls here
+
+
+urlpatterns = [
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('register/', views.register_view, name='register_view'),
+]
